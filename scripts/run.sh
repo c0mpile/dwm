ab04ab90fcc550cb48a28084a49e8df428cac1af
Kevin <kevin@chroot.us>
2025-03-17 15:33:35 -0400

    added picom config

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+xrandr --output DP-1 --primary --mode 2560x1440 --rate 165 --output DP-2 --mode 2560x1440 --rate 165 --right-of DP-1
 xrdb merge ~/.Xresources 
 #xbacklight -set 10 &
 feh --bg-fill ~/Pictures/wallpaper &
