793051c1b91bfc4171007362d0a67b252b7219db
c0mpile <zsh@disroot.org>
2025-03-17 15:00:55 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 xrdb merge ~/.Xresources 
-xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wall/gruv.png &
+#xbacklight -set 10 &
+feh --bg-fill ~/Pictures/wallpaper &
 xset r rate 200 50 &
 picom &
 
-dash ~/.config/dwm/scripts/bar.sh &
+bash ~/.config/dwm/scripts/bar.sh &
 while type dwm >/dev/null; do dwm && continue || break; done
